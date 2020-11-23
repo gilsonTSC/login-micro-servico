911fc9943fb1e89a51a4c9843850dbaf304895bd
Gilson Trajano <gilson.costa@dce.ufpb.br>
2020-11-23 11:03:34 -0300

    Implementando o envio do login com o rabbitMQ

---
+++ src/main/java/com/br/myfood/login/message/LoginReceiveMessage.java
@@ -0,0 +1,28 @@
+package com.br.myfood.login.message;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.br.myfood.login.dto.LoginDto;
+import com.br.myfood.login.entity.Login;
+import com.br.myfood.login.repository.LoginRepository;
+
+@Component
+public class LoginReceiveMessage {
+
+    private final LoginRepository loginRepository;
+
+    @Autowired
+    public LoginReceiveMessage(LoginRepository loginRepository) {
+        this.loginRepository = loginRepository;
+    }
+
+    @RabbitListener(queues = {"${cadastro.login.rabbitmq.queue}"})
+    public void receive(@Payload LoginDto loginDto) {
+        System.out.println(loginDto);
+        loginRepository.save(new Login(null, loginDto.getEmail(), loginDto.getPassword()));
+    }
+
+}
