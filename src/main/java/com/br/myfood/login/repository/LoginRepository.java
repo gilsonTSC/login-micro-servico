a44a4d2be8847f45b5de646e88d4c6ab727bec85
Gilson Trajano <gilson.costa@dce.ufpb.br>
2020-11-23 13:31:28 -0300

    Validando o login na base de dados

--- src/main/java/com/br/myfood/login/repository/LoginRepository.java
+++ src/main/java/com/br/myfood/login/repository/LoginRepository.java
@@ -1,6 +1,9 @@
 package com.br.myfood.login.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.br.myfood.login.entity.Login;
@@ -8,4 +11,7 @@ import com.br.myfood.login.entity.Login;
 @Repository
 public interface LoginRepository extends JpaRepository<Login, Long>{
 
+	@Query("SELECT l FROM Login l WHERE l.email = ? AND l.password = ?")
+	Optional<Login> findByEmailAndPassword(String email, String password);
+	
 }
